9452d8254507cd30a1ceeff25949edf357d7ff67
saeedmaghdam <me.register@outlook.com>
2021-08-08 23:51:18 +0430

    #1 Initialized the service.

---
+++ src/eCommerceAutomation/ProductUpdaterJob.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DashFire;
+using Microsoft.Extensions.Logging;
+
+namespace eCommerceAutomation
+{
+    public class ProductUpdaterJob : Job
+    {
+        private readonly ILogger<ProductUpdaterJob> _logger;
+
+        public override JobInformation JobInformation => JobInformationBuilder.CreateInstance()
+            .RegistrationRequired()
+            .SetCronSchedules(new[] { "0 */3 * * *" })
+            .SetDescription("Fetch sources, analyze them and update product!")
+            .SetDisplayName("Product Updater Job")
+            .SetSystemName(nameof(ProductUpdaterJob))
+            .Build();
+
+        public ProductUpdaterJob(ILogger<ProductUpdaterJob> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override Task StartInternallyAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/eCommerceAutomation/Program.cs
@@ -0,0 +1,20 @@
+﻿using DashFire;
+using Microsoft.Extensions.Hosting;
+
+namespace eCommerceAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddJob<ProductUpdaterJob>();
+                })
+                .UseDashFire()
+                .Build()
+                .Run();
+        }
+    }
+}
